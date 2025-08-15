06a70eeb3e1cf4f676fb34443a53b6bc12a0cabb
Fred Pikkov <fpikkov@gmail.com>
2025-08-15 15:54:26 +0300

    Took some notes to thin aboout later

--- cpp09/ex02/PmergeMe.hpp
+++ cpp09/ex02/PmergeMe.hpp
@@ -9,9 +9,10 @@
 #include <sstream>
 #include <stdexcept>
 
-// How many contained elements to print out.
-static constexpr bool			PRINT_LIMIT	= true;
-static constexpr size_t const	PRINT_MAX	= 5;
+// Constants
+static constexpr bool	const	COMPARISON_COUNTER	= true;
+static constexpr bool	const	PRINT_LIMIT			= true;
+static constexpr size_t const	PRINT_MAX			= 5;
 
 /**
  * @brief Concept which checks (at compile time) that the container passed to us
@@ -44,6 +45,7 @@ class PmergeMe
 		// ---------------- Member variables -----------
 		Container<T>	_sequence;
 		duration		_time;
+		size_t			_comparisons;
 
 		// ---------------- Storage --------------------
 		void containerize( const std::string& values )
@@ -60,14 +62,15 @@ class PmergeMe
 	public:
 		// ---------------- Rule of five ----------------
 		PmergeMe() = delete;
-		PmergeMe( const std::string& sequence )	{ containerize(sequence); }
+		PmergeMe( const std::string& sequence ) : _comparisons(0)	{ containerize(sequence); }
 		PmergeMe( const PmergeMe& ) = delete;
 		PmergeMe& operator=( const PmergeMe& ) = delete;
 		~PmergeMe() {}
 
 		// ---------------- Getters ---------------------
-		const Container&	getSequence()	const	{ return (_sequence); }
-		const duration&		getTime()		const	{ return (_time); }
+		const Container&	getSequence()		const	{ return (_sequence); }
+		const duration&		getTime()			const	{ return (_time); }
+		size_t				getComparisons()	const	{ return (_comparisons); }
 
 		// ---------------- Helpers ---------------------
 		void	printTime( const std::string& containerType ) const
@@ -97,9 +100,49 @@ class PmergeMe
 		}
 
 		// ---------------- Sorting ---------------------
+		// On entry the container may be the non-const reference to mainChain
 		void	mergeInsert( Container& seq )
 		{
+			// NOTE: Do I flush and construct a new 'seq' here?
+			if (seq.size() <= 1)
+				return ;
 
+			Container<T>			mainChain;
+			Container<T>			pending;
+			Container<pair_type>	pairs;
+
+			// Make pairs out of sequence
+			for ( auto it : seq )
+			{
+				auto itx = std::next(it);
+				if ( itx == seq.end() )
+					break ;
+				// Compare and swap positions so first < second
+				pairs.push_back(std::make_pair(*it, *itx));
+				auto lastPair = pairs.back();
+				if (lastPair.first > lastPair.second)
+					std::swap(lastPair.first, lastPair.second);
+				if constexpr (COMPARISON_COUNTER)
+					++_comparisons;
+
+				// NOTE: Do I pass the mainChain recursively or flush seq and reuse that?
+				mainChain.push_back(lastPair.second);
+			}
+
+			/**
+			 * NOTE: The 'seq' container should be flushed but first we need to keep track
+			 * of the first elements in pairs but also any leftovers which become the pending chain.
+			 */
+			// Recursive call (branching point)
+			mergeInsert(mainChain);
+
+			/**
+			 * NOTE: how should I track which pair ended up as the first one during recursion?
+			 */
+
+			// Push the leftover element into pending after returning from recursion
+			if (seq.size() % 2)
+				pending.push_back(seq.back());
 		}
 };
 
